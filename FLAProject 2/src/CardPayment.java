06eaf77475385adff24544a392ad98fc252bbffa
Devarell <devarell090@gmail.com>
2025-05-07 10:26:20 +0700

    CardPayment.java

--- FLAProject 2/src/CardPayment.java
+++ FLAProject 2/src/CardPayment.java
@@ -3,6 +3,9 @@ package business;
 public class CardPayment implements PaymentStrategy {
     @Override
     public void pay(double amount) {
-        System.out.println("Paid $" + amount + " using Card.");
+        double discountedAmount = amount * 0.8; // 20% discount
+        System.out.println("Original amount: $" + String.format("%.2f", amount));
+        System.out.println("After 20% discount: $" + String.format("%.2f", discountedAmount));
+        System.out.println("Paid $" + String.format("%.2f", discountedAmount) + " using Card.");
     }
 }
