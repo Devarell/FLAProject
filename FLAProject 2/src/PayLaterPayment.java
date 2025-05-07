c48476ce99357579f5cf327475f37d403d92fc79
Devarell <devarell090@gmail.com>
2025-05-07 10:29:55 +0700

    PayLaterPayment.java

--- FLAProject 2/src/PayLaterPayment.java
+++ FLAProject 2/src/PayLaterPayment.java
@@ -4,5 +4,9 @@ public class PayLaterPayment implements PaymentStrategy {
     @Override
     public void pay(double amount) {
         System.out.println("Payment of $" + amount + " scheduled using PayLater.");
+        double ExtraFees = amount * 1.2; // 20% extra fees
+        System.out.println("Original amount: $" + String.format("%.2f", amount));
+        System.out.println("After 20% extra fees: $" + String.format("%.2f", ExtraFees));
+        System.out.println("Paid $" + String.format("%.2f", ExtraFees) + " using PayLater.");
     }
 }
