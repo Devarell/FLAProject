b08090a3b67fb48d4a58ed7d30a7fdf2a1a552f7
Devarell <devarell090@gmail.com>
2025-05-07 10:27:43 +0700

    Update Food.java

--- FLAProject 2/src/Food.java
+++ FLAProject 2/src/Food.java
@@ -2,8 +2,26 @@ package presentation;
 
 public abstract class Food {
     protected String name;
+    protected double price;
+    protected int quantity;
 
     public String getName() {
         return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return price * quantity;
     }
 }
