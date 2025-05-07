2418ce61a1a9876514f25b24196e54e0ffbfee03
Devarell <devarell090@gmail.com>
2025-05-07 10:28:23 +0700

    FoodFactory.java

--- FLAProject 2/src/FoodFactory.java
+++ FLAProject 2/src/FoodFactory.java
@@ -3,26 +3,33 @@ package presentation;
 import presentation.Food;
 
 public class FoodFactory {
-    public static Food createFood(String type) {
+    public static Food createFood(String type, int quantity) {
+        Food food;
         switch (type.toLowerCase()) {
             case "pizza":
-                return new Pizza();
+                food = new Pizza();
+                break;
             case "burger":
-                return new Burger();
+                food = new Burger();
+                break;
             default:
                 throw new IllegalArgumentException("Unknown food type: " + type);
         }
+        food.setQuantity(quantity);
+        return food;
     }
 }
 
 class Pizza extends Food {
     public Pizza() {
         this.name = "Pizza";
+        this.price = 15.99;
     }
 }
 
 class Burger extends Food {
     public Burger() {
         this.name = "Burger";
+        this.price = 8.99;
     }
 }
